4a1a6f3db2a8996b64f151b478c4c1d5a09fe8b1
VladykaTMY <82215415+VladykaTMY@users.noreply.github.com>
2021-05-26 12:01:36 +0300

    обновлен

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter, NavLink, Route} from 'react-router-dom';
-import Profile from "./componets/Profile"
+import Profile from "./componets/Profile";
 import './App.css';
-import Friends from "./componets/Friends"
+import Friends from "./componets/Friends";
 
 const Messenges = ()=>{
-  return <h1>Страница с сообщениями</h1>
+  return <h1>Страница с сообщениями</h1>;
 }
 
 const Settings = ()=>{
-  return <h1>Страница с настройками</h1>
+  return <h1>Страница с настройками</h1>;
 }
 
 
@@ -18,32 +18,35 @@ const Menu =()=>{
   return(
     <div >
       <div className="nav flex-column nav-pills" aria-orientation="vertical">
-        <NavLink className="nav-link" to="profile">Профиль</NavLink>
-        <NavLink className="nav-link" to="messanges">Сообщения</NavLink>
-        <NavLink className="nav-link" to="settings">Настройки</NavLink>
-        <NavLink className="nav-link" to="friends">Друзья</NavLink>
+        <NavLink className="nav-link" to="/profile">Профиль</NavLink>
+        <NavLink className="nav-link" to="/messanges">Сообщения</NavLink>
+        <NavLink className="nav-link" to="/settings">Настройки</NavLink>
+        <NavLink className="nav-link" to="/friends">Друзья</NavLink>
       </div>
   </div>
-  )
-}
+  );
+};
 
 function App(props) {
   console.log(props)
   return (
-  <div className="container-fluid">
+  <div className="container-fluid mt-4">
     <BrowserRouter>
     <div className="row">
       <div className="col-sm-3">
         <Menu />
       </div>
       <div className="col-sm-9">
-        
-          <Route path="/profile" render={()=><Profile function ={props.functions.key_getUser}/>} />
+          <Route
+           path="/profile"
+           render={()=><Profile function ={props.functions.key_getUser}/>}
+          />
           <Route path="/messanges" component={Messenges}/>
           <Route path="/settings" component={Settings}/>
-          <Route path="/friends" render={()=><Friends function ={props.functions.key_getUsers}/>} />
-
-        
+          <Route
+           path="/friends"
+           render={()=><Friends function ={props.functions.key_getUsers}/>} 
+          />
       </div>
     </div>
     </BrowserRouter>
